1fc2fb1127f91b313a983bceaf0d1a88a957f402
othonasval <104383804+othonasval@users.noreply.github.com>
2022-08-28 11:41:01 +0300

    Update solver.cpp

--- solver.cpp
+++ solver.cpp
@@ -7,16 +7,10 @@
 int main(){
     
 
-    CombustionChamber CC(0.25*pi*pow(15.56e-3,2),0.0001,"ChamberPressure.dat",2.5);
+    CombustionChamber dummyCC(0.25*pi*pow(15.56e-3,2),0.0001,"ChamberPressure.dat",2.5);
     
     //Nozzle dummyNozzle("ChamberPressure.dat",0.25*pi*pow(15.56e-3,2),4, "exit_pressure_sup.dat", "exit_pressure_NSE.dat", "thrust.dat");  
    
-    
-
-
-    
-    
-
     return 0;
 
 }
